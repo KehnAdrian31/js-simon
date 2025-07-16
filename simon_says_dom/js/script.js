d57e5260e337b5a7f28d1705bf672f9d4f0c6279
KehnAdrian31 <kehn000panganibangmail.com>
2025-07-16 21:19:17 +0200

    [upd] recupero gli elementi del dom

---
+++ simon_says_dom/js/script.js
@@ -0,0 +1,8 @@
+// recuperare gli elementi del dom che ci servono
+const numbersList = document.getElementById(`numbers-list`);
+const form = document.getElementById(`answers-form`);
+const inputs = document.guerySelectorAll(`input`);
+const countdown = document.getElementById(`countdown`);
+const message = document.getElementById(`message`);
+const instructions = document.getElementById(`instructions`);
+const button = document.querySelector(`button`);
